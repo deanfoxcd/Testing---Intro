4d00f2aaaef8085953af9cd1c47c839d691b7e8d
Dean Fox <deanfoxcd@gmail.com>
2025-03-03 15:15:09 -0500

    initial commit and initial tests

---
+++ src/App.test.js
@@ -0,0 +1,24 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import user from '@testing-library/user-event';
+import App from './App';
+
+test('shows 6 productes by default', async () => {
+  render(<App />);
+
+  const titles = await screen.findAllByRole('heading');
+  expect(titles).toHaveLength(6);
+});
+
+test('clicking button shows 6 more products', async () => {
+  render(<App />);
+
+  const button = await screen.findByRole('button', {
+    name: /load more/i,
+  });
+  user.click(button);
+
+  await waitFor(async () => {
+    const titles = await screen.findByRole('heading');
+    expect(titles).toHaveLength(12);
+  });
+});
